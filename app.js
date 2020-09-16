2ee80f10e7c774a87c3faad023a4b6b47d53b3e5
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-16 04:05:59 -0700

    Commiting changes

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ let count = 0;
 
 // select value and buttons
 const value = document.getElementById('#value');
-const btns = document.querySelectorAll('btn');
+const btns = document.querySelectorAll('.btn');
 
 btns.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
     const styles = e.currentTarget.classList;
-    if (styles.contain('decrease')) {
+    if (styles.contains('decrease')) {
       count--;
     }
+    value.textContent = count;
   });
 });
